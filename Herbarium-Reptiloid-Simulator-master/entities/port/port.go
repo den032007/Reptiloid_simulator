d44ed99d487138d0532011c18a3d1b6a45c9427d
Denis <Adeptuscromwell@gmail.com>
2018-10-26 04:53:16 +0300

    storyline. pics, text, new ships added

--- Herbarium-Reptiloid-Simulator-master/entities/port/port.go
+++ Herbarium-Reptiloid-Simulator-master/entities/port/port.go
@@ -105,3 +105,21 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "jumper_factory"
+  type: "collectionfactory"
+  data: "prototype: \"/entities/ship/jumper_ship.collection\"\n"
+  "load_dynamically: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
